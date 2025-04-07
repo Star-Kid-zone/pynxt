e8e7914603dd2c1fc2ff4c0dd572d9c69071cf37
star kid <vimalrajan555.m@gmail.com>
2025-04-07 11:20:06 +0530

    again

--- python/todo_app/app/db/mongo.py
+++ python/todo_app/app/db/mongo.py
@@ -1,4 +1,3 @@
-# app/db/mongo.py
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
@@ -7,4 +6,4 @@ MONGO_URI = settings.MONGO_URL
 DATABASE_NAME = settings.DATABASE_NAME
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client[DATABASE_NAME]  # ✅ THIS is what you're trying to import
+db = client[DATABASE_NAME]  
